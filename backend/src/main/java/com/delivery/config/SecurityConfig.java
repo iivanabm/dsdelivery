25c57ab6fe9d77d4c1d675e01c89766cac7dc703
iivanabm <ivanammarinho@hotmail.com>
2021-01-06 10:39:32 -0300

    Ready to prod

--- backend/src/main/java/com/delivery/config/SecurityConfig.java
+++ backend/src/main/java/com/delivery/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.delivery.config;
 
 import java.util.Arrays;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
