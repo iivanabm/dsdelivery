c2aed198792d5beca03017bf69c8928b060253a1
iivanabm <ivanammarinho@hotmail.com>
2021-01-08 09:08:23 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,14 @@
 import './App.css';
+import "./App.css";
 import Navbar from './Navbar';
+import Home from "./Home";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    </div >
+      <Home />
+    </>
   );
 }
 
